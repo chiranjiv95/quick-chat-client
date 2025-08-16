aaac6bb8b84c1079bd24eac210d3baf13b885568
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2025-08-16 10:50:41 +0530

    signup page initial UI

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,5 +1,64 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submitted", user);
+  };
+
+  return (
+    <div>
+      <h2>Create Account</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Firstname"
+          name="firstname"
+          value={user.firstname}
+          onChange={handleOnChange}
+        />
+        <input
+          type="text"
+          placeholder="Lastname"
+          name="lastname"
+          value={user.lastname}
+          onChange={handleOnChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={user.email}
+          onChange={handleOnChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={user.password}
+          name="password"
+          onChange={handleOnChange}
+        />
+        <button type="submit">Signup</button>
+      </form>
+      <div>
+        <span>
+          Already have an account? <Link to="/login">Login</Link> here
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
