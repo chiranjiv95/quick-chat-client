76fb214c6e8b764dbf0177a23f0bc6ad5bd69a45
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2025-08-16 11:10:40 +0530

    login page initial UI

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("data", data);
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <input
+          type="password"
+          name="password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <span>
+          Don't have an account yet? <Link to="/signup">Signup</Link> here
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
