a7ac8804b1905cae94e14d1d1247b6b3342158ca
chiranjiv95 <chiranjiv.mohanty10@gmail.com>
2025-08-17 08:00:48 +0530

    show user info in home screen

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,10 +3,34 @@ import { useSelector } from "react-redux";
 const HomePage = () => {
   const { user = {} } = useSelector((state) => state.userReducer);
 
+  const getFullname = () => {
+    if (!user) return "";
+
+    const firstname =
+      user.firstname?.charAt(0).toUpperCase() +
+      user.firstname?.slice(1).toLowerCase();
+
+    const lastname =
+      user.lastname?.charAt(0).toUpperCase() +
+      user.lastname?.slice(1).toLowerCase();
+
+    return firstname + " " + lastname;
+  };
+
+  const getInitials = () => {
+    if (!user) return "";
+
+    const first = user.firstname?.[0]?.toUpperCase() || "";
+    const last = user.lastname?.[0]?.toUpperCase() || "";
+
+    return first + last;
+  };
+
   return (
     <div>
       Home Page
-      <h2>Hi, {user?.firstname}!</h2>
+      <h2>Hi, {getFullname()}!</h2>
+      <p>{getInitials()}</p>
     </div>
   );
 };
